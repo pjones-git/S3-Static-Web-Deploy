8851c9592175bd9865bcebdb58bbfa12f89724e0
pjones-git <138961397+pjones-git@users.noreply.github.com>
2026-02-11 09:17:59 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,14 @@
+output "bucket_name" {
+  value       = aws_s3_bucket.website.bucket
+  description = "Name of the S3 bucket hosting the site"
+}
 
+output "website_endpoint" {
+  value       = aws_s3_bucket_website_configuration.website.website_endpoint
+  description = "S3 static website endpoint (HTTP)"
+}
+
+output "website_url" {
+  value       = "http://${aws_s3_bucket_website_configuration.website.website_endpoint}"
+  description = "Full website URL"
+}
