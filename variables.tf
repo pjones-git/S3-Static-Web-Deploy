1bbecc20b98d89e0c9ef2e71f68ad670030b0456
pjones-git <138961397+pjones-git@users.noreply.github.com>
2026-02-11 09:30:21 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "aws_region" {
   description = "AWS region to deploy into (e.g., us-east-1)"
   type        = string
+  default     = "us-east-1"
 }
 
 # --- Option A (Recommended): Terraform Cloud OIDC assume role ---
@@ -8,6 +9,7 @@ variable "aws_region" {
 variable "assume_role_arn" {
   description = "IAM Role ARN to assume (recommended with Terraform Cloud OIDC)"
   type        = string
+  default     = "arn:aws:iam::219342442719:role/TerraformDev"
 }
 
 
