62b90ab95d902155c34003c151b142e40cc790e5
pjones-git <138961397+pjones-git@users.noreply.github.com>
2026-02-11 08:59:13 -0600

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,1 +1,24 @@
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5"
+    }
+  }
+
+  # OPTIONAL (Recommended): If you are using Terraform Cloud workspace "S3-Static-Web-Deploy"
+  # Uncomment and set your org name.
+  #
+  # cloud {
+  #   organization = "LXTCH"
+  #   workspaces {
+  #     name = "S3-Static-Web-Deploy"
+  #   }
+  # }
+}
