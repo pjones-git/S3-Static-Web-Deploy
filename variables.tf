98acda646e1f13e33dca6ac1565863a5a5485a50
pjones-git <138961397+pjones-git@users.noreply.github.com>
2026-02-11 09:16:51 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "aws_region" {
   description = "AWS region to deploy into (e.g., us-east-1)"
   type        = string
+  default     = "us-east-1"
 }
 
 # --- Option A (Recommended): Terraform Cloud OIDC assume role ---
