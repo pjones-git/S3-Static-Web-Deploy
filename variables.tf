a742cf5074a0283403b089579b6ab9bdd2588dca
pjones-git <138961397+pjones-git@users.noreply.github.com>
2026-02-11 09:16:18 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "aws_region" {
 # --- Option A (Recommended): Terraform Cloud OIDC assume role ---
 # If you are running in Terraform Cloud with OIDC, you typically do NOT set access keys.
 variable "assume_role_arn" {
-  description = "IAM Role ARN to assume (recommended with Terraform Cloud OIDC). Leave empty if not using assume_role."
+  description = "IAM Role ARN to assume (recommended with Terraform Cloud OIDC)"
   type        = string
   default     = "arn:aws:iam::219342442719:role/TerraformDev"
 }
